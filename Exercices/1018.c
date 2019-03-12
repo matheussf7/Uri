c2cde44f0a4da019dbb8e56039b4e448b774b189
aluno <aluno@unipe.local>
2019-03-12 20:49:27 -0300

    Primeiros exercicios

---
+++ Exercices/1018.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+//1018
+int main(){
+	int quant;
+	scanf("%d", &quant);
+	
+	printf("%d\n", quant);
+	
+	printf("%d nota(s) de R$ 100,00\n", ((quant - quant % 100)/100));
+	printf("%d nota(s) de R$ 50,00\n", )
+	
+	return 0;
+}
